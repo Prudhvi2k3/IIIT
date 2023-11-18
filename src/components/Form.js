83f15566b693d7dd0766b955d2e743b04128e787
Prudhvi2k3 <prudhviankamreddi1@gmail.com>
2023-11-18 15:19:13 +0530

    modified:   src/components/Form.css 	modified:   src/components/Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -45,6 +45,22 @@ const VolunteerForm = () => {
               onChange={handleChange}
               required
             />
+            <label htmlFor="volunteerDay">Select Day:</label>
+            <select
+              id="volunteerDay"
+              name="volunteerDay"
+              value={formData.volunteerDay}
+              onChange={handleChange}
+              required
+            >
+              <option value="monday">Monday</option>
+              <option value="tuesday">Tuesday</option>
+              <option value="wednesday">Wednesday</option>
+              <option value="thursday">Thursday</option>
+              <option value="friday">Friday</option>
+              <option value="saturday">Saturday</option>
+              <option value="sunday">Sunday</option>
+            </select>
           </fieldset>
 
           <fieldset>
